b8422f626ddb8de84bb57d7c654f9edc40d426a7
Alex Tetervak <tetervak@sheridancollege.ca>
2020-01-30 08:48:52 -0500

    initial commit

---
+++ js/flower.js
@@ -0,0 +1,15 @@
+/* Alex Tetervak, Sheridan College, Ontario */
+
+"use strict"; // use the strict mode
+
+// starts using jQuery
+$(function() {
+
+    let flower = JSON.parse(localStorage["flower"]);
+    $("span.flower_name").text(flower.label);
+    $("span.flower_price").text(flower.price);
+    $("img.flower_image")
+        .attr("src",`images/flowers/${flower.pictures.large}`)
+        .attr("alt",flower.label);
+    $("p.flower_info").text(flower.text);
+});
---
+++ js/main.js
@@ -0,0 +1,41 @@
+/* Alex Tetervak, Sheridan College, Ontario */
+
+"use strict"; // use the strict mode
+
+// starts using jQuery
+$(function() {
+    // load the xml file
+    $.getJSON("data/catalog.json",displayFlowers);
+});
+
+function displayFlowers(data) {
+    let $table = $("<table></table>");
+    $table.appendTo("main");
+    let flowers = data.flowers;
+    flowers.forEach(function (flower, index) {
+            $table.append(
+                `<tr> 
+                <td>
+                    <a href="flower.html" data-index="${index}">
+                     <img src="images/flowers/${flower.pictures.small}" alt="${flower.name}">
+                    </a> 
+                </td> 
+                <td>
+                    <span class="flower_label">${flower.label}</span>&nbsp;&nbsp;
+                    <span class="flower_price">${flower.price}</span><br>
+                    ${flower.text}
+                </td>
+            </tr>`
+            );
+        }
+    );
+
+    $("table a").click(function(){
+        let index = $(this).data("index");
+        localStorage["flower"] = JSON.stringify(flowers[index]);
+    });
+}
+
+
+
+
